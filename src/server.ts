70b838990061f9ee2763ee793907c1a2376f14c2
Karthikeya.Gundumogula <karthikeya200226@gmail.com>
2023-02-15 20:55:50 +0530

    Subraph Of Ad-Server Project

--- src/server.ts
+++ src/server.ts
@@ -1,236 +1,206 @@
 import {
   AdCreated as AdCreatedEvent,
   AdServed as AdServedEvent,
-  ApprovalForAll as ApprovalForAllEvent,
   CampaignStarted as CampaignStartedEvent,
   CampaignStopped as CampaignStoppedEvent,
   Click as ClickEvent,
   PublisherAdded as PublisherAddedEvent,
   PublisherCreated as PublisherCreatedEvent,
   PublisherRemoved as PublisherRemovedEvent,
-  TransferBatch as TransferBatchEvent,
-  TransferSingle as TransferSingleEvent,
-  URI as URIEvent,
   fundsAdded as fundsAddedEvent,
-  fundsRemoved as fundsRemovedEvent
-} from "../generated/Server/Server"
-import {
-  AdCreated,
-  AdServed,
-  ApprovalForAll,
-  CampaignStarted,
-  CampaignStopped,
-  Click,
-  PublisherAdded,
-  PublisherCreated,
-  PublisherRemoved,
-  TransferBatch,
-  TransferSingle,
-  URI,
-  fundsAdded,
-  fundsRemoved
-} from "../generated/schema"
+  fundsRemoved as fundsRemovedEvent,
+} from "../generated/Server/Server";
+import { BigInt } from "@graphprotocol/graph-ts";
+import { Ad, Advertiser, Publisher } from "../generated/schema";
 
 export function handleAdCreated(event: AdCreatedEvent): void {
-  let entity = new AdCreated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.id = event.params.id
-  entity.totalFunds = event.params.totalFunds
-  entity.AdURI = event.params.AdURI
-  entity.Advertiser = event.params.Advertiser
+  let entity = new Ad(event.params.id.toString());
+  entity.id = event.params.id.toString();
+  entity.AdId = event.params.id;
+  entity.Advertiser = event.params.Advertiser;
+  entity.TotalFunds = event.params.totalFunds;
+  entity.TotalClicks = new BigInt(0);
+  entity.TotalViews = new BigInt(0);
+  entity.Publishers = new Array<BigInt>();
+  entity.AdImg = event.params.AdURI;
+  entity.isRunning = false;
+  let AdvertiserEntity = Advertiser.load(event.params.Advertiser.toString());
+  if (AdvertiserEntity == null) {
+    AdvertiserEntity = new Advertiser(event.params.Advertiser.toString());
+    AdvertiserEntity.id = event.params.Advertiser.toString();
+    AdvertiserEntity.Address = event.params.Advertiser;
+    AdvertiserEntity.TotalSpent = BigInt.fromString("0");
+    AdvertiserEntity.AdIds = new Array<BigInt>();
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+    AdvertiserEntity.BlockNumber = event.block.number;
+    AdvertiserEntity.BlockTimestamp = event.block.timestamp;
+    AdvertiserEntity.TransactionHash = event.transaction.hash;
+    AdvertiserEntity.save();
+  }
+  let AdIds = AdvertiserEntity.AdIds;
+  AdIds.push(event.params.id);
+  AdvertiserEntity.AdIds = AdIds;
+  AdvertiserEntity.save();
 
-  entity.save()
-}
+  entity.BlockNumber = event.block.number;
+  entity.BlockTimestamp = event.block.timestamp;
+  entity.TransactionHash = event.transaction.hash;
 
-export function handleAdServed(event: AdServedEvent): void {
-  let entity = new AdServed(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.Adid = event.params.Adid
-  entity.publisherId = event.params.publisherId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleApprovalForAll(event: ApprovalForAllEvent): void {
-  let entity = new ApprovalForAll(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.account = event.params.account
-  entity.operator = event.params.operator
-  entity.approved = event.params.approved
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleCampaignStarted(event: CampaignStartedEvent): void {
-  let entity = new CampaignStarted(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.id = event.params.id
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleCampaignStopped(event: CampaignStoppedEvent): void {
-  let entity = new CampaignStopped(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.id = event.params.id
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleClick(event: ClickEvent): void {
-  let entity = new Click(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.id = event.params.id
-  entity.PublisherId = event.params.PublisherId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handlePublisherAdded(event: PublisherAddedEvent): void {
-  let entity = new PublisherAdded(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.id = event.params.id
-  entity.PublisherId = event.params.PublisherId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  entity.save();
 }
 
 export function handlePublisherCreated(event: PublisherCreatedEvent): void {
-  let entity = new PublisherCreated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.id = event.params.id
-  entity.clickReward = event.params.clickReward
-  entity.displayReward = event.params.displayReward
-  entity.publisher = event.params.publisher
-  entity.url = event.params.url
+  let entity = new Publisher(event.params.id.toString());
+  entity.id = event.params.id.toString();
+  entity.PublisherId = event.params.id;
+  entity.Publisher = event.params.publisher;
+  entity.TotalEarnings = new BigInt(0);
+  entity.TotalClicks = new BigInt(0);
+  entity.TotalViews = new BigInt(0);
+  entity.Advertisers = new Array<BigInt>();
+  entity.PublisherSite = event.params.url;
+  entity.ClickReward = event.params.clickReward;
+  entity.ViewReward = event.params.displayReward;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.BlockNumber = event.block.number;
+  entity.BlockTimestamp = event.block.timestamp;
+  entity.TransactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
+}
+
+export function handleCampaignStarted(event: CampaignStartedEvent): void {
+  let entity = Ad.load(event.params.id.toString());
+  if (entity != null) {
+    entity.isRunning = true;
+    entity.save();
+  } else {
+    return;
+  }
+}
+
+export function handlePublisherAdded(event: PublisherAddedEvent): void {
+  let entity = Ad.load(event.params.id.toString());
+  let pubEntity = Publisher.load(event.params.PublisherId.toString());
+  if (entity != null && pubEntity != null) {
+    let newPublishers = entity.Publishers;
+    newPublishers.push(event.params.PublisherId);
+    entity.Publishers = newPublishers;
+    entity.save();
+    let newAdvertisers = pubEntity.Advertisers;
+    newAdvertisers.push(event.params.id);
+    pubEntity.Advertisers = newAdvertisers;
+    pubEntity.save();
+  } else {
+    return;
+  }
+}
+
+export function handleAdServed(event: AdServedEvent): void {
+  let entity = Ad.load(event.params.Adid.toString());
+  let pubEntity = Publisher.load(event.params.publisherId.toString());
+  if (entity != null && pubEntity != null) {
+    let AdvertiserEntity = Advertiser.load(entity.Advertiser.toString());
+    let inc = BigInt.fromString("1");
+    let totalViews = entity.TotalViews;
+    totalViews = totalViews.plus(inc);
+    entity.TotalViews = totalViews;
+    let totalFunds = entity.TotalFunds;
+    totalFunds = totalFunds.minus(pubEntity.ViewReward);
+    entity.TotalFunds = totalFunds;
+    entity.save();
+    totalViews = pubEntity.TotalViews;
+    totalViews = totalViews.plus(inc);
+    pubEntity.TotalViews = totalViews;
+    let reward = pubEntity.ViewReward;
+    pubEntity.TotalEarnings = pubEntity.TotalEarnings.plus(reward);
+    if (AdvertiserEntity != null) {
+      AdvertiserEntity.TotalSpent = AdvertiserEntity.TotalSpent.plus(reward);
+      AdvertiserEntity.save();
+    }
+    pubEntity.save();
+  } else {
+    return;
+  }
+}
+
+export function handleClick(event: ClickEvent): void {
+  let entity = Ad.load(event.params.id.toString());
+  let pubEntity = Publisher.load(event.params.PublisherId.toString());
+  if (entity != null && pubEntity != null) {
+    let AdvertiserEntity = Advertiser.load(entity.Advertiser.toString());
+    let inc = BigInt.fromString("1");
+    let totalClicks = entity.TotalClicks;
+    totalClicks = totalClicks.plus(inc);
+    entity.TotalClicks = totalClicks;
+    entity.TotalFunds = entity.TotalFunds.minus(pubEntity.ClickReward);
+    totalClicks = pubEntity.TotalClicks;
+    totalClicks = totalClicks.plus(inc);
+    pubEntity.TotalClicks = totalClicks;
+    let reward = pubEntity.ClickReward;
+    pubEntity.TotalEarnings = pubEntity.TotalEarnings.plus(reward);
+    let index = pubEntity.Advertisers.indexOf(event.params.id);
+    if (index <= -1) {
+      let newAdvertisers = pubEntity.Advertisers;
+      newAdvertisers.push(event.params.id);
+      pubEntity.Advertisers = newAdvertisers;
+    }
+    if (AdvertiserEntity != null) {
+      AdvertiserEntity.TotalSpent = AdvertiserEntity.TotalSpent.plus(reward);
+      AdvertiserEntity.save();
+    }
+    entity.save();
+    pubEntity.save();
+  } else {
+    return;
+  }
+}
+
+export function handleCampaignStopped(event: CampaignStoppedEvent): void {
+  let entity = Ad.load(event.params.id.toString());
+  if (entity != null) {
+    entity.isRunning = false;
+    entity.save();
+  } else {
+    return;
+  }
 }
 
 export function handlePublisherRemoved(event: PublisherRemovedEvent): void {
-  let entity = new PublisherRemoved(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.id = event.params.id
-  entity.PublisherId = event.params.PublisherId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleTransferBatch(event: TransferBatchEvent): void {
-  let entity = new TransferBatch(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.operator = event.params.operator
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.ids = event.params.ids
-  entity.values = event.params.values
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleTransferSingle(event: TransferSingleEvent): void {
-  let entity = new TransferSingle(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.operator = event.params.operator
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.id = event.params.id
-  entity.value = event.params.value
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleURI(event: URIEvent): void {
-  let entity = new URI(event.transaction.hash.concatI32(event.logIndex.toI32()))
-  entity.value = event.params.value
-  entity.id = event.params.id
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let entity = Ad.load(event.params.id.toString());
+  let pubEntity = Publisher.load(event.params.PublisherId.toString());
+  if (entity != null && pubEntity != null) {
+    let newPublishers = entity.Publishers;
+    let index = newPublishers.indexOf(event.params.PublisherId);
+    newPublishers.splice(index, 1);
+    entity.Publishers = newPublishers;
+    let newAdvertisers = pubEntity.Advertisers;
+    index = newAdvertisers.indexOf(event.params.id);
+    newAdvertisers.splice(index, 1);
+    pubEntity.Advertisers = newAdvertisers;
+    pubEntity.save();
+    entity.save();
+  } else {
+    return;
+  }
 }
 
 export function handlefundsAdded(event: fundsAddedEvent): void {
-  let entity = new fundsAdded(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.id = event.params.id
-  entity.AddedAmount = event.params.AddedAmount
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let entity = Ad.load(event.params.id.toString());
+  if (entity != null) {
+    entity.TotalFunds = entity.TotalFunds.plus(event.params.AddedAmount);
+    entity.save();
+  } else {
+    return;
+  }
 }
 
 export function handlefundsRemoved(event: fundsRemovedEvent): void {
-  let entity = new fundsRemoved(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.id = event.params.id
-  entity.RemovedAmount = event.params.RemovedAmount
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let entity = Ad.load(event.params.id.toString());
+  if (entity != null) {
+    entity.TotalFunds = entity.TotalFunds.minus(event.params.RemovedAmount);
+    entity.save();
+  } else {
+    return;
+  }
 }
